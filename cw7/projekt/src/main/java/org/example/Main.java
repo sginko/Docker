53b99d0952934461c2b1d2dfffa6e84324fd2d5c
sginko <s.ginkota@gmail.com>
2024-07-26 14:58:33 +0200

    add cw7

---
+++ cw7/projekt/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "Hello";
+        System.out.println(StringUtils.upperCase(message));
+    }
+}
